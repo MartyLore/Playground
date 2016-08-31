013a21ddfa965bfb2f885f2644bd23314ba7b598
jonnyblu <j0nny.3lu@googlemail.com>
2016-09-01 00:08:02 +0200

    /form

--- node_apps/myapp/routes/index.js
+++ node_apps/myapp/routes/index.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET form*/
+router.get('/form', function(req, res) {
+	res.render('form');
+});
+
+// POST form
+router.post('/submitForm', function(req, res) {
+	var name = req.body.inputName;
+	var nickname = req.body.inputNickname;
+	var email = req.body.inputEmail;
+
+	res.render('hallo', {name:name, nickname: nickname});
 });
 
 module.exports = router;
